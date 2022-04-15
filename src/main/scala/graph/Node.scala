a6f22d5f84280b0f4c39ac15cdcf859e53d507de
zdulak <60813197+zdulak@users.noreply.github.com>
2022-04-15 19:58:25 +0200

    Add toString method for Node class

--- src/main/scala/graph/Node.scala
+++ src/main/scala/graph/Node.scala
@@ -1,5 +1,24 @@
 package graph
 
-class Node {
+case class Node(name: String, children: Set[Node] = Set.empty) {
+  def add(node: Node): Node = if (node == this) this else Node(name, children + node)
+
+  def remove(node: Node): Node = Node(name, children - node)
+
+  def display(): Unit = println(toString)
+
+  override def toString: String = {
+    def graphToString(children: Set[Node], visitedNodes: Set[Node], offset: Int, sb: StringBuilder): String = {
+      val nodesToVisit = children.diff(visitedNodes)
+      for (node <- nodesToVisit) {
+        sb ++= " " * offset ++= "+- " ++= node.name ++= "\n"
+        graphToString(node.children, visitedNodes + node, offset + 3, sb)
+      }
+
+      sb.toString()
+    }
+
+    graphToString(children, Set(this), 0, new StringBuilder(name + "\n"))
+  }
 
 }
